df831b3d731678650bd2c710ec0e3bfed56312dc
meghana.s <meghana.s@perfios.com>
2024-12-19 23:58:34 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function () {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area').addClass('navbar_fixed');
+                } else {
+                    $('.header_area').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
+});
